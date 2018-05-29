9442350231eeffa721a2ddaecf27b2f8a36d8455
Radi <radi.rusadi.s@gmail.com>
2018-05-29 17:09:06 +0700

    Logging user in

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import firebase from "firebase";
 import { Button, Card, CardSection, Input } from "./common";
 
 class LoginForm extends Component {
   state = { email: "", password: "" };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password);
+  }
 
   render() {
     return (
@@ -17,8 +23,8 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Input        
-            secureTextEntry    
+          <Input
+            secureTextEntry
             placeholder="password"
             label="Pasword  :"
             value={this.state.password}
@@ -27,7 +33,7 @@ class LoginForm extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>Log In</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
         </CardSection>
       </Card>
     );
