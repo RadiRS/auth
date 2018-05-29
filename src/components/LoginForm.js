c21c54bf991d73c0ca7ac7aec0da5b577b6d9d13
Radi <radi.rusadi.s@gmail.com>
2018-05-29 15:37:57 +0700

    Handling User Input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, TextInput } from "react-native";
 import { Button, Card, CardSection } from "./common";
 
 class LoginForm extends Component {
   render() {
     return (
       <Card>
+        <CardSection>
+          <TextInput style={{ height: 40, width: 100 }} />
+        </CardSection>
         <CardSection />
-        <CardSection />
-        
+
         <CardSection>
           <Button>Log In</Button>
         </CardSection>
